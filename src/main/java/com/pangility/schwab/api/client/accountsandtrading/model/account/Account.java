2c30e2daac49b70f2b3bcc869459ba6c3dc16cdf
gporter <greg.porter70@gmail.com>
2024-05-31 09:26:19 -0500

    Fixed problem with Account missing "otherFields".  Schwab added a field to the account service and it was breaking when it shouldn't have been.

--- src/main/java/com/pangility/schwab/api/client/accountsandtrading/model/account/Account.java
+++ src/main/java/com/pangility/schwab/api/client/accountsandtrading/model/account/Account.java
@@ -1,9 +1,14 @@
 package com.pangility.schwab.api.client.accountsandtrading.model.account;
 
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Account
@@ -15,4 +20,7 @@ import lombok.ToString;
 @JsonInclude(JsonInclude.Include.NON_EMPTY)
 public class Account {
     private SecuritiesAccount securitiesAccount;
+    @JsonIgnore
+    @JsonAnySetter
+    private Map<String, Object> otherFields = new HashMap<>();
 }
